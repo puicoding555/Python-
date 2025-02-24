f33b88d7e6329d8df1b07c0f72d8291f1b12e71a
Seksan Thichon <seksan1467@gmail.com>
2025-02-24 11:37:28 +0700

    Update Task4.py

--- Lab Read Write/Task4.py
+++ Lab Read Write/Task4.py
@@ -1,6 +1,5 @@
 with open(r"C:\Users\CSIT\Desktop\PythonFile.txt", 'r') as file:
     content = file.read()
-# ต่อท้ายคำว่า "ไพทอน"
 content += " test"
 
 # เขียนข้อมูลที่ได้ลงในไฟล์ "filePython_2.txt"
